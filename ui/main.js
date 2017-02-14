c5c89283bf60ce683cb125c13604475e2862bc0b
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-14 19:58:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element=document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginlelt=marginLeft+'px';//...
+}
 img.onclick=function()
 {
-    img.style.marginLeft='200px';
+    var interval=setInterval(moveRight,100);
 };
