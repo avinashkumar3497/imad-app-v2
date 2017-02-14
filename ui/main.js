8b48bbefc39baab90d814011b6c9a5281771c975
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-14 19:30:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');    //print the "Loaded!" in console, which you can see through the google chrome inspect element feature(developer tool)
+//change the text of the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
+//move the image
+var img=document.getElementById(madi);
+img.onclick=function()
+{
+    image.style.marginLeft='100px';
+}
