ae289375cc3563baebc66f3ff33059e1a9ab48c6
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-14 19:24:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,3 @@
-console.log('Loaded!');
+console.log('Loaded!');    //print the "Loaded!" in console, which you can see through the google chrome inspect element feature(developer tool)
+var element=document.getElementById('main-text');
+element.innerHTML='NEW VALUE';
