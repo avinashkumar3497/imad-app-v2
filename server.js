d1ac2052bbbd06d7f232e56dcf4121d48bc8dc43
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-14 07:09:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,21 +3,83 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 app.use(morgan('combined'));
-
+var articles={
+    'article-one':{
+        title:' Article-one|Avinash',
+        heading:'Article One',
+        date:'feb 13,2017',
+        content:    `
+                    <p>
+                        This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. 
+                        </p>
+                     <p>
+                        This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. 
+                    </p>
+                     <p>
+                        This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. This is the content of my first article, that is article one. 
+                    </p>`
+    },//used back-quote instead of single quote
+   'article-two':{
+       title:' Article-two|Avinash',
+        heading:'Article Two',
+        date:'feb 14,2017',
+        content:    `//used back-quote instead of single quote
+                    <p>
+                    This is the content of the article-two.
+                    </p>`
+   },
+   'article-three':{
+        title:' Article-three|Avinash',
+        heading:'Article three',
+        date:'feb 15,2017',
+        content:    `//used back-quote instead of single quote
+                    <p>
+                    This is the content of the article-three.
+                    </p>`
+   }
+};
+function createTemplate(data)//used back-quote instead of single quote
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+            ${heading}
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>`
+;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName',function(req,res)     //: is the feature of the morgan
+{  //articleName = article-one
+    //article[articleName]={} content object for object one
+    articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
