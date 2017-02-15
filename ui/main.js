faa6923b85000586d7e6d7054b6d4137fce15eec
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-15 19:23:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,4 @@
-
-var ctr=4;
+var button= document.getElementById('counter');
 console.log('Loaded!');    //print the "Loaded!" in console, which you can see through the google chrome inspect element feature(developer tool)
 //change the text of the main-text div
 var element=document.getElementById('main-text');
@@ -16,11 +15,32 @@ img.onclick=function()
 {
     var interval=setInterval(moveRight,100);
 };
-//for the footer botton
+button.onclick=function()
+{
+    var request= new XMLHttpRequest();   // object 'request' created
+//Capture the response and store it in a variable
+request.onreadystatechange=function()
+{
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+            var ctr=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=ctr.toString();
+        }
+    }
+}
+}
+//Make the request
+request.open('GET','http://avinashkumar3497.imad.hasura-app.io/counter',true); //constructor of object 'request'
+request.send(null);
+/*for the footer botton
+var ctr=4;
 var button =document.getElementById('counter');
 button.onclick=function()
 {
     ctr++;
     var span=document.getElementById('count');
     span.innerHTML=ctr.toString();
-};
+};*/
