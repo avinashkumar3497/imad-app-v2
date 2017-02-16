7b99fb48d587f9ace84f39f8ff55f5129ba634a8
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-16 08:23:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,10 +93,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var names=[];
-app.get('/submit-name/:name',function(req,res)
+app.get('/submit-name',function(req,res)    // URL:/submit-name?name=xxxxxx
 {
     //get the name from the request
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
