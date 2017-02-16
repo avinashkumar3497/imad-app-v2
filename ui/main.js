a092954731fed78726f6e2feeb582e3fb12d8b57
avinashkumar3497 <avinashkumar3497@gmail.com>
2017-02-16 09:43:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,21 +35,38 @@ request.onreadystatechange=function()
 request.open('GET','http://avinashkumar3497.imad.hasura-app.io/counter',true); /*. It will make the request and will get get us the response from http://avinashkumar3497.imad.hasura-app.io/counter*/
 request.send(null);
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
     //make a request to the server and send the name
     //Capture the list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>' + names[i] + '</li>';
-    }
-    var ui=document.getElementById('namelist');
-    ui.innerHTML=list;
+                var request= new XMLHttpRequest();   // object 'request' created, constructor will be called.
+            //Capture the response and store it in a variable
+            request.onreadystatechange=function()
+            {
+                if(request.readyState===XMLHttpRequest.DONE)
+                {
+                    if(request.status===200)
+                    {
+                        var names=request.responseText;
+                        names=JSON.parse(names);
+                        var list='';
+                        for(var i=0;i<names.length;i++)
+                        {
+                            list+='<li>' + names[i] + '</li>';
+                        }
+                         var ui=document.getElementById('namelist');
+                        ui.innerHTML=list; 
+                    }
+                }
+            }
+            //Make the request
+             request.open('GET','http://avinashkumar3497.imad.hasura-app.io/submit-name?name=' + name,true); /*. It will make the request and will get get us the response from http://avinashkumar3497.imad.hasura-app.io/counter*/
+            request.send(null);     
+             
 };
 /*for the footer botton
 var ctr=4;
